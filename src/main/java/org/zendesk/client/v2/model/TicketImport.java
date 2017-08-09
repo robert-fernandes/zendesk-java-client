95b048bada5356fca13546d06705a83fdd3dfe21
rfernandes <rfernandes@netflix.com>
2017-08-09 10:38:38 -0700

    Set initial serial UUID to 1L, per recommended contribution doc

--- src/main/java/org/zendesk/client/v2/model/TicketImport.java
+++ src/main/java/org/zendesk/client/v2/model/TicketImport.java
@@ -10,8 +10,8 @@ import java.util.List;
 @JsonIgnoreProperties(value = "comment", ignoreUnknown = true)
 public class TicketImport extends Ticket {
 
-    private static final long serialVersionUID = -5670864013402830387L;
-    
+    private static final long serialVersionUID = 1L;
+
     private List<Comment> comments;
 
     public TicketImport() {
